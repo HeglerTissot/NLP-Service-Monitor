ad1ece990cb300d8fd45bd7d77a33d002f6de1c7
Michael-Dyq <68020340+Michael-Dyq@users.noreply.github.com>
2021-06-10 10:18:45 -0400

    add demos on leguin

--- Interface/js/interface.js
+++ Interface/js/interface.js
@@ -97,13 +97,20 @@ function checkStatus(){
 	timestampElement = document.getElementById("lastTimestamp");
 	timestampElement.innerHTML = new Date();
     
-    console.log(fetchINFO('https://cogcomp.seas.upenn.edu/lg4029/info','status_multipackage'));
 	console.log(fetchINFO('https://cogcomp.seas.upenn.edu/dc4007/info','status_zeroshot'));
 	console.log(fetchINFO('https://cogcomp.seas.upenn.edu/dc4031/info','status_multilang_edl'));
 	console.log(fetchINFO('https://cogcomp.seas.upenn.edu/dc4032/info','status_multilang_ner'));
     console.log(fetchINFO('https://cogcomp.seas.upenn.edu/dc4037/info','status_srl_english_frontend'));
     console.log(fetchINFO('https://cogcomp.seas.upenn.edu/dc4039/info','status_srl_english_backend'));
     console.log(fetchINFO('https://cogcomp.seas.upenn.edu/dc4038/info','status_srl_spanish_backend'));
+    
+    console.log(fetchINFO('https://cogcomp.seas.upenn.edu/lg4021/info','status_kairos_events_frontend'));
+    console.log(fetchINFO('https://cogcomp.seas.upenn.edu/lg4023/info','status_kairos_events_backend'));
+    console.log(fetchINFO('https://cogcomp.seas.upenn.edu/lg4024/info','status_kairos_temporal_backend'));
+    console.log(fetchINFO('https://cogcomp.seas.upenn.edu/lg4025/info','status_kairos_storyline_backend'));
+    console.log(fetchINFO('https://cogcomp.seas.upenn.edu/lg4029/info','status_multipackage')); 
+    console.log(fetchINFO('https://cogcomp.seas.upenn.edu/lg4039/info','status_srl_backend_leguin'));
+
 }
 
 function fetchINFO(url,elementId) {
