ef97bf25759574af0bce7be16e60951a2ae8bd47
Michael-Dyq <68020340+Michael-Dyq@users.noreply.github.com>
2021-06-19 12:36:06 -0400

    Add login function

--- Interface/js/interface.js
+++ Interface/js/interface.js
@@ -1,88 +1,88 @@
 
-async function postInfo_old(demoUrl, elementId) {
-    statusElement = document.getElementById(elementId)
-    try {
-        //alert(demoUrl);
-        fetch(demoUrl, {
-            method: 'POST',
-            cache: 'no-cache',
-            headers: {
-                'Accept': "application/json, text/plain, */*",
-                'Content-Type': "application/json;charset=utf-8"
-            },
-            mode: 'no-cors',
-            body: JSON.stringify({})
-        }).then(resp => resp.json())
-            .then(json_output => {
-                try {
-                    statusElement.innerText = json_output.status;
-                    alert(json_output.status);
-                } catch(err) {
-                    statusElement.innerText = 'offline';
-                    alert(err.message);
-                }
-            }
-        );
-       // .catch(function() {
-       //     statusElement.innerText = 'offline';
-       // }
+// async function postInfo_old(demoUrl, elementId) {
+//     statusElement = document.getElementById(elementId)
+//     try {
+//         //alert(demoUrl);
+//         fetch(demoUrl, {
+//             method: 'POST',
+//             cache: 'no-cache',
+//             headers: {
+//                 'Accept': "application/json, text/plain, */*",
+//                 'Content-Type': "application/json;charset=utf-8"
+//             },
+//             mode: 'no-cors',
+//             body: JSON.stringify({})
+//         }).then(resp => resp.json())
+//             .then(json_output => {
+//                 try {
+//                     statusElement.innerText = json_output.status;
+//                     alert(json_output.status);
+//                 } catch(err) {
+//                     statusElement.innerText = 'offline';
+//                     alert(err.message);
+//                 }
+//             }
+//         );
+//        // .catch(function() {
+//        //     statusElement.innerText = 'offline';
+//        // }
         
     
-    } catch(err) {
-        statusElement.innerText = 'offline';
-        alert(err.message);
-    }
-}
+//     } catch(err) {
+//         statusElement.innerText = 'offline';
+//         alert(err.message);
+//     }
+// }
 
-async function postInfo(demoUrl, elementId) {
-    statusElement = document.getElementById(elementId);
-    try {
-        fetch(demoUrl, {
-            method: 'POST',
-            cache: 'no-cache',
-            headers: {
-                'Accept': "application/json, text/plain, */*",
-                'Content-Type': "application/json;charset=utf-8"
-            },
-            mode: 'no-cors',
-            body: JSON.stringify({})
-        }).then(resp => resp.json())
-            .then(json_output => {
-                try {
-                    statusElement.innerText = json_output.status;
-                } catch(err) {
-                    statusElement.innerText = 'offline';
-                }
-            });
-        //.catch(function() {
-         //   statusElement.innerText = 'offline';
-         //alert('!!!!');
-        //});
+// async function postInfo(demoUrl, elementId) {
+//     statusElement = document.getElementById(elementId);
+//     try {
+//         fetch(demoUrl, {
+//             method: 'POST',
+//             cache: 'no-cache',
+//             headers: {
+//                 'Accept': "application/json, text/plain, */*",
+//                 'Content-Type': "application/json;charset=utf-8"
+//             },
+//             mode: 'no-cors',
+//             body: JSON.stringify({})
+//         }).then(resp => resp.json())
+//             .then(json_output => {
+//                 try {
+//                     statusElement.innerText = json_output.status;
+//                 } catch(err) {
+//                     statusElement.innerText = 'offline';
+//                 }
+//             });
+//         //.catch(function() {
+//          //   statusElement.innerText = 'offline';
+//          //alert('!!!!');
+//         //});
 
-    } catch(err) {
-        statusElement.innerText = 'offline';
-    }
-}
+//     } catch(err) {
+//         statusElement.innerText = 'offline';
+//     }
+// }
 
-async function postHalt() {
-    fetch('http://localhost:8081/halt', {
-        method: 'POST',
-        cache: 'no-cache',
-        headers: {
-            'Accept': "application/json, text/plain, */*",
-            'Content-Type': "application/json;charset=utf-8"
-        },
-        // mode: 'no-cors',
-        body: JSON.stringify({})
-    }).then(resp => resp.json())
-        .then(json_output => {
-            // noting
-        }
-    )
-    .catch(function() {
-        // nothing;
-    });
-}
+// async function postHalt() {
+//     fetch('http://localhost:8081/halt', {
+//         method: 'POST',
+//         cache: 'no-cache',
+//         headers: {
+//             'Accept': "application/json, text/plain, */*",
+//             'Content-Type': "application/json;charset=utf-8"
+//         },
+//         // mode: 'no-cors',
+//         body: JSON.stringify({})
+//     }).then(resp => resp.json())
+//         .then(json_output => {
+//             // noting
+//         }
+//     )
+//     .catch(function() {
+//         // nothing;
+//     });
+// }
 
 //-------------------------------- Useful Functions ---------------------------------
 
@@ -94,13 +94,35 @@ function reStart(url, elementId){
   }
 
 function reStart_pwd_2(url, elementId){
-    var pwd = prompt("Password:");
-    var url2 = url +"?password="+ pwd;
-    statusElement = document.getElementById(elementId);
-    alert('If your password is correct, this service will be restarted now!');
-    statusElement.innerHTML = 'Checking...';
-    fetch(url2);  
+  var url2 = url +"?password="+ pwd;
+  statusElement = document.getElementById(elementId);
+  alert('If your password is correct, this service will be restarted now!');
+  statusElement.innerHTML = 'Checking...';
+  fetch(url2);  
 }
+
+function login() {
+    var pwd = prompt("Password:");
+    var url = 'https://cogcomp.seas.upenn.edu/dc4050/loginCheck' +"?password="+ pwd;
+    fetch(url)
+    .then((response) => response.json())
+    .then((info) => { 
+    if(info == "correct"){
+        alert('Sucessfully login!');
+        console.log(info);
+    }else{
+        alert("The password is incorrect!")
+        location.reload();
+    }
+    })
+    .catch((error) => {
+        console.warn(url);
+        console.warn(error);
+        alert("Something seems wrong... Please contact service administrator!")
+		window.location.replace("https://cogcomp.seas.upenn.edu/page/demos/");
+	});
+}
+ 
 
 function reStart_pwd(url, elementId){
     var pwd = prompt("Password:");
