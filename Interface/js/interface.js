67901b55eebe89dc4428ed7e8673fe3d6d757ac4
Michael-Dyq <68020340+Michael-Dyq@users.noreply.github.com>
2021-07-20 12:06:25 -0400

    Decorated the restart page

--- Interface/js/interface.js
+++ Interface/js/interface.js
@@ -183,12 +183,14 @@ function fetchINFO(url,elementId) {
 		console.log(info);
 		document.getElementById(elementId).innerHTML = 'Online';
         //document.getElementById(elementId).style.color = 'green';
+        document.getElementById(elementId).className = 'btn btn-success';
     })
     .catch((error) => {
         console.warn(url);
 		console.warn(error);
 		document.getElementById(elementId).innerHTML = 'Offline';
         //document.getElementById(elementId).style.color = 'red';
+        document.getElementById(elementId).className = 'btn btn-danger';
 	});
 }
 
